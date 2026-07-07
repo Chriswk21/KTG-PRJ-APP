72db5d94590feb547495fc9e7451989460e23c7d
Chriswk21 <chriswk2103@gmail.com>
2026-07-07 13:26:07 +0700

    Replace PWA icons with Kopi Tubruk Gadjah logo and set type to image/png

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/jpeg'
+            type: 'image/png'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/jpeg'
+            type: 'image/png'
           }
         ]
       }
